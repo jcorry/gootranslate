442f58cc4462611ac97f8182f21f76c884b3d6b4
John Corry <john@yikyakapp.com>
2016-03-25 09:15:07 -0400

    travis config

---
+++ tests/GooTranslate.spec.php
@@ -0,0 +1,35 @@
+<?php
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Middleware;
+
+describe('GooTranslate', function(){
+    describe('when the constructor is called', function(){
+        beforeEach(function(){
+            $mock = new Client([
+                'headers' => [
+                    'content-type' => 'application/json'
+                ],
+                'base_uri' => 'https://www.googleapis.com/language/translate/v2/',
+                'query' => ['key' => env('GOOGLE_API_KEY')]
+            ]);
+
+            $this->cut = new \Jcorry\GooTranslate\GooTranslateClass($mock);
+        });
+
+        it("should accept a GuzzleHttp\\Client as it's single param", function(){
+            expect($this->cut)->to->not->be->null();
+        });
+    });
+
+    describe('methods', function(){
+        describe('languages', function(){});
+        describe('translate', function(){});
+        describe('detect', function(){});
+    });
+});
